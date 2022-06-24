de772c2b78aea731e837a20e70503cc00d811f08
Mitchell Kager <mitchellkager@gmail.com>
2022-06-24 02:37:52 -0700

    oop correct kwargs

--- scripts/txt2img_delegant.py
+++ scripts/txt2img_delegant.py
@@ -47,7 +47,7 @@ def process_queue(thread_id, s3, sqs, model, sampler):
 
                 logging.info(f'RECV {uid}: "{prompt}"')
 
-                generate(prompt, _path_from_uid(uid), model, sampler, sample_path=None)
+                generate(prompt, _path_from_uid(uid), model, sampler, sampledir=None)
         else:
             logging.info('Thread sees no new messages -- sleeping')
             time.sleep(RETRY_DELAY)
